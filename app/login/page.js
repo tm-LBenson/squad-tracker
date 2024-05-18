64966041a4eb646dabc26508715aa491f2dc7ab5
Lewis Benson <tm.lewis.benson@gmail.com>
2024-05-17 21:59:53 -0400

    add login splash page

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+import LoginPage from '../components/LoginPage';
+
+export default function page() {
+  return (
+    <main>
+      <LoginPage />
+    </main>
+  );
+}
