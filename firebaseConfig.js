88f1433b2f7b794165e350663b13a9e985b88c5f
Lewis Benson <tm.lewis.benson@gmail.com>
2024-05-18 10:00:21 -0400

    comment analytics

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -19,5 +19,5 @@ const app = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 export { db, auth, provider };
